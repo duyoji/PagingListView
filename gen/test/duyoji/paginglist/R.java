ea5750ecf49061dadfa26f5340e2957ce2128e98
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2013-02-23 12:27:34 +0900

    modified target api8 (SdkVersion=8)

--- gen/test/duyoji/paginglist/R.java
+++ gen/test/duyoji/paginglist/R.java
@@ -15,16 +15,12 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int item_paging_list_image=0x7f070000;
-        public static final int main_list=0x7f070001;
-        public static final int menu_settings=0x7f070002;
+        public static final int item_paging_list_image=0x7f060000;
+        public static final int main_list=0x7f060001;
     }
     public static final class layout {
         public static final int item_paging_list=0x7f030000;
         public static final int main=0x7f030001;
-    }
-    public static final class menu {
-        public static final int main=0x7f060000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
